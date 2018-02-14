9abb4b9cc47743b61ea40bae55a666e6a7ae65af
ChristopherGustafson <36486430+ChristopherGustafson@users.noreply.github.com>
2018-02-14 22:31:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,4 +3,15 @@
 
 int main(void) {
 	
+	/* Set up peripheral bus clock */
+	OSCCON &= ~0x180000;
+	OSCCON |= 0x080000;
+	
+	/* Set button 2-4 to input */
+	TRISD |= (3 << 5);
+	
+	/* Set LEDS to output */
+	TRISE &= ~0xFF;
+	
+	
 }
