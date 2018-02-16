3db01f3401a46a8cbaa565e42755f53fe94adc3d
gubbilubbi <curry470>
2018-02-16 15:59:01 +0100

    Main klar

--- main.c
+++ main.c
@@ -7,16 +7,22 @@ int main(void) {
 
 	int running = 0;
 
+	//TAGET FRÅN LAB 3 mipslabmain.c
 	/* Set up peripheral bus clock */
 	/* Sätter klockan till 80 Mhz*/
-	OSCCON &= ~0x180000;
-	OSCCON |= 0x080000;
+	SYSKEY = 0xAA996655;  /* Unlock OSCCON, step 1 */
+	SYSKEY = 0x556699AA;  /* Unlock OSCCON, step 2 */
+	while(OSCCON & (1 << 21)); /* Wait until PBDIV ready */
+	OSCCONCLR = 0x180000; /* clear PBDIV bit <0,1> */
+	while(OSCCON & (1 << 21));  /* Wait until PBDIV ready */
+	SYSKEY = 0x0;  /* Lock OSCCON */
 
 	/* Set button 2-4 to input */
-	TRISD |= (3 << 5);
+	TRISD |= (2 << 5);
 
 	/* Set LEDS to output */
 	TRISE &= ~0xFF;
+
 
 
 	/* Pointless comment */
