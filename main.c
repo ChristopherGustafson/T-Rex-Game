f09badd451f94062965cada4f7584c9d77c54dfe
gubbilubbi <curry470>
2018-02-16 12:59:00 +0100

    fixed

--- main.c
+++ main.c
@@ -4,13 +4,9 @@
 
 
 int main(void) {
-<<<<<<< HEAD
 
-=======
-	
 	int running = 0;
-	
->>>>>>> 4592b5f5cafd060301cd6b62d7d64cd9f1ac1ce2
+
 	/* Set up peripheral bus clock */
 	/* Sätter klockan till 80 Mhz*/
 	OSCCON &= ~0x180000;
@@ -21,19 +17,13 @@ int main(void) {
 
 	/* Set LEDS to output */
 	TRISE &= ~0xFF;
-<<<<<<< HEAD
 
 
-=======
-	
 	/* Pointless comment */
-	
-	
-	
+
+
+
 	initGame();
 	running = 1;
-	
-	
-	
->>>>>>> 4592b5f5cafd060301cd6b62d7d64cd9f1ac1ce2
+
 }
