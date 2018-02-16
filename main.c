0e68b0a5d3fbb89115adede5ab5871f9ea05701a
gubbilubbi <curry470>
2018-02-16 12:55:48 +0100

    hej

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 #include <pic32mx.h>
 
 int main(void) {
-	
+
 	/* Set up peripheral bus clock */
+	/* Sätter klockan till 80 Mhz*/
 	OSCCON &= ~0x180000;
 	OSCCON |= 0x080000;
-	
+
 	/* Set button 2-4 to input */
 	TRISD |= (3 << 5);
-	
+
 	/* Set LEDS to output */
 	TRISE &= ~0xFF;
-	
-	
+
+
 }
